ef9174f2dbcead5522ca201f4e03098a74fd966a
Rustam Izmaylov <izmaylov-r@hotmail.com>
2022-09-11 21:56:39 +0300

    Added Product Details

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,23 +4,48 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using StoneStore.Data;
 using StoneStore.Models;
+using StoneStore.Models.ViewModels;
 
 namespace StoneStore.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly StoneDbContext _db;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, StoneDbContext db)
         {
+            _db = db;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
-            return View();
+            HomeVM homeVM = new HomeVM()
+            {
+                Products = _db.Product.Include(u => u.Category).Include(u => u.ApplicationType),
+                Categories = _db.Category
+            };
+                
+            return View(homeVM);
+        }
+
+        public IActionResult Details(int id)
+        {
+            DetailsVM DetailsVM = new DetailsVM()
+            {
+                Product = _db.Product
+                    .Include(u => u.Category)
+                    .Include(u => u.ApplicationType)
+                    .FirstOrDefault(u => u.Id == id),
+                ExistsInCart = false
+            };
+
+            return View(DetailsVM);
         }
 
         public IActionResult Privacy()
