9b4dd89605eac7bb862bc78b6084b20a99dfd884
Rustam Izmaylov <izmaylov-r@hotmail.com>
2023-03-26 12:29:19 +0300

    fix

--- Models/ViewModels/HomeVM.cs
+++ Models/ViewModels/HomeVM.cs
@@ -6,5 +6,7 @@ namespace StoneStore.Models.ViewModels
     public class HomeVM
     {
         public IEnumerable<Product> Products { get; set; }
+        
+        public IEnumerable<Category> Categories { get; set; }
     }
 }
