1d74e11e40b9942eba5d9be14d590a0e46c9f929
Rustam Izmaylov <izmaylov-r@hotmail.com>
2023-03-29 20:34:22 +0300

    added summary

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using StoneStore.Data;
 using StoneStore.Models;
+using StoneStore.Models.ViewModels;
 using StoneStore.Utility;
 
 namespace StoneStore.Controllers
 {
+    [Authorize]
     public class CartController : Controller
     {
+        [BindProperty]
+        public ProductUserVM ProductUserVm { get; set; }
         private readonly StoneDbContext _db;
         
         public CartController(StoneDbContext db)
@@ -29,6 +35,56 @@ namespace StoneStore.Controllers
             IEnumerable<Product> prodList = _db.Product.Where(u => prodInCart.Contains(u.Id));
             
             return View(prodList);
+        }
+        
+        public IActionResult Remove(int id)
+        {
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Any())
+            {
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+
+            shoppingCartList.Remove(shoppingCartList.FirstOrDefault(u => u.ProductId == id));
+
+            HttpContext.Session.Set(WC.SessionCart, shoppingCartList);
+            
+            return RedirectToAction(nameof(Index));
+        }
+        
+        [HttpPost]
+        [IgnoreAntiforgeryToken]
+        [ActionName("Index")]
+        public IActionResult IndexPost()
+        {
+            return RedirectToAction(nameof(Summary));
+        }
+        
+        public IActionResult Summary()
+        {
+
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            // var userId = User.FindFirstValue(ClaimTypes.Name);
+            
+            
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Any())
+            {
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+
+            List<int> prodInCart = shoppingCartList.Select(i => i.ProductId).ToList();
+            IEnumerable<Product> prodList = _db.Product.Where(u => prodInCart.Contains(u.Id));
+
+            ProductUserVm = new ProductUserVM()
+            {
+                ApplicationUser = _db.ApplicationUser.FirstOrDefault(u => u.Id == claim.Value)
+            };
+            
+            return View(ProductUserVm);
         }
     }
 }
---
+++ Models/ViewModels/ProductUserVM.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace StoneStore.Models.ViewModels
+{
+    public class ProductUserVM
+    {
+        public ApplicationUser ApplicationUser { get; set; }
+        public IEnumerable<Product> ProductList { get; set; }
+
+
+        public ProductUserVM()
+        {
+            ProductList = new List<Product>();
+        }
+    }
+}
